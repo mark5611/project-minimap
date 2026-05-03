43f50836399efc367f74efd2e04fa13787208482
mark5611 <mark.kertesz@outlook.hu>
2026-05-03 18:34:25 +0200

    basic DB

--- OSMMapExtractor.py
+++ OSMMapExtractor.py
@@ -1,3 +1,5 @@
+import time
+
 import osmium
 import json
 from pathlib import Path
@@ -26,7 +28,12 @@ class MyHandler(osmium.SimpleHandler):
         self.processed_items += 1
 
         if self.processed_items % self.progress_step == 0:
-            print(f"\rProgress: ~{self.processed_items} elements", end="", flush=True)
+            print(f"\rProcessing OSM Data.", end="", flush=True)
+            time.sleep(0.1)
+            print(f"\rProcessing OSM Data..", end="", flush=True)
+            time.sleep(0.1)
+            print(f"\rProcessing OSM Data...", end="", flush=True)
+            time.sleep(0.1)
 
     def way(self, w):
         self.update_progress()
